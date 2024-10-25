d5d2c576c0b823a23cd1dcb3859b0780e32c376f
Taha Ahmad <craftingcaptain456@gmail.com>
2024-10-25 18:24:38 +0500

    Styling changes
    - Stopped stylix from theming spicetify, nvim and tmux
    - Added custom Retro theme for spicetify
    - Changed tmux & nvim catppuccin flavour to macchiato from mocha

--- config/nvim/plugins/catppuccin.lua
+++ config/nvim/plugins/catppuccin.lua
@@ -2,7 +2,7 @@ require("catppuccin").setup({
 	flavour = "mocha", -- latte, frappe, macchiato, mocha
 	background = { -- :h background
 		light = "latte",
-		dark = "mocha",
+		dark = "macchiato",
 	},
 	transparent_background = true,
 	show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
