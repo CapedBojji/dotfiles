4349877e0dfe4bba3b25b42eee3f353a58a318eb
blackbojji <blackbojji@gmail.com>
2025-10-05 19:51:43 -0400

    fix: update roblox template to use jecs-luau path and description

--- templates/default.nix
+++ templates/default.nix
@@ -8,8 +8,8 @@
     path = ./rust/pure;
     description = "Pure Rust project template (devenv shells: default, rust)";
   };
-  roblox = {
-    path = ./roblox;
-    description = "Roblox game development template with pesde, rojo, darklua, and stylua";
+  roblox-jecs-luau = {
+    path = ./roblox/jecs/luau;
+    description = "JECS Luau project template (devenv shell: jecs-luau)";
   };
 }
