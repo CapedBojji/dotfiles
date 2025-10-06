d515e527cee369a1208d9d2c0f894292ef06004e
blackbojji <blackbojji@gmail.com>
2025-10-05 20:06:18 -0400

    Squashing unnecessary Copilot commits.

--- overlays/modifications.nix
+++ overlays/modifications.nix
@@ -1,7 +1,6 @@
 # Override or tweak existing packages here
-final: prev: let
-in {
+final: prev: {
   luau = prev.callPackage ../pkgs/luau/default.nix {
-    inherit (prev) stdenv fetchFromGitHub cmake ninja pkg-conf openssl zlib lib;
+    inherit (prev) stdenv fetchFromGitHub cmake ninja pkg-config openssl zlib lib;
   };
 }
--- templates/roblox/jecs/luau-test/src/main.lua
+++
@@ -1,1 +0,0 @@
-print("Hello from JECS Luau test project!")
